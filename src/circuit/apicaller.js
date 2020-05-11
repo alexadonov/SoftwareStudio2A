bb8241f9839774043366dd63ad08f2840a0629f4
justinsaji <justinsaji1@gmail.com>
2020-05-11 14:08:37 +1000

    Added register & login api calls to apicaller.js

--- src/circuit/apicaller.js
+++ src/circuit/apicaller.js
@@ -1,5 +1,5 @@
 export const saveCircuit = async (student_id, circuit_name, circuit_input, circuit_output_json) => {
-    const url = "http://13.211.191.11:8000/api/save-circuit";
+    const url = "http://13.210.43.87:8000/api/save-circuit";
     var data = JSON.stringify({
         student_id: student_id,
         circuit_name: circuit_name,
@@ -26,8 +26,56 @@ export const saveCircuit = async (student_id, circuit_name, circuit_input, circu
     */
 }
 
+export const register = async(student_id, is_admin, confirm_admin, first_name, last_name, email, password) => {
+    const url = "http://13.210.43.87:8000/api/register";
+    var data = JSON.stringify({
+        student_id: student_id,
+        is_admin: is_admin,
+        confirm_admin: confirm_admin,
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        password: password
+    });
+    
+    var response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: data
+    });
+
+    var status = await response.status;
+    console.log('data:', data);
+    console.log('status:', status);   
+    return status == "201";
+}
+
+export const login = async(email, password) => {
+    const url = "http://13.210.43.87:8000/api/login";
+    var data = JSON.stringify({
+        email: email,
+        password: password
+    });
+    
+    var response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: data
+    });
+
+    var status = await response.status;
+    console.log('data:', data);
+    console.log('status:', status);   
+    return status == "200";
+}
+
 export const getResults = async (circuit_input) => {
-    const url = "http://127.0.0.1:8000/api/calculate";
+    const url = "http://13.210.43.87:8000/api/calculate";
+    //const url = "http://127.0.0.1:8000/api/calculate";
     var data = JSON.stringify({
         circuit_input: JSON.stringify(circuit_input)
     });
@@ -48,19 +96,17 @@ export const getResults = async (circuit_input) => {
     if (status == "200")
         return parsedData;
     else
-    // Handle error here
-        return;
-    
+        return; // Handle error here
 }
 
 export const healthCheck = async () => {
-    const url = "http://127.0.0.1:8000/api/";
+    const url = "http://13.210.43.87:8000/api/";
 
     var response = await fetch(url, {
         method: 'GET'
     });
 
     var status = await response.status;
-    console.log('response:', response);
-    console.log('status:', status);
+    console.log('health response:', response);
+    console.log('health status:', status);
 }
