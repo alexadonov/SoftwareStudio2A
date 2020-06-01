ee448f84f264664918e8b0daf623333cdeae052e
georgee99 <41958847+georgee99@users.noreply.github.com>
2020-06-01 12:10:26 +1000

    Update results.js

--- src/components/results.js
+++ src/components/results.js
@@ -38,7 +38,8 @@ export default class Results extends Component {
             labels: this.state.labels,
             datasets: [{
               label: 'Probability',
-              data: this.state.probs
+              data: this.state.probs,
+              backgroundColor:'rgba(90, 170, 255)'
             }]
           }}
           width={this.props.width}
@@ -48,6 +49,40 @@ export default class Results extends Component {
               display: true,
               position: 'bottom'
             },
+            tooltips: {
+              callbacks:{
+                title: function(tooltipItems, data) {
+                  return 'Details'; /*Details isn't a good word */
+                },
+
+                beforeLabel: function(tooltipItems, data) { 
+                  return 'Decimal: ' + parseInt(tooltipItems.xLabel, 2);               
+                },
+
+                label: function(tooltipItems, data) {  
+                  var a = tooltipItems.yLabel              
+                  var aRnd = a.toFixed(3)
+                return 'Probability: ' + aRnd;   
+                },
+
+                afterLabel: function(tooltipItems, data){
+                  var mag = Math.sqrt(tooltipItems.yLabel)
+                  var magRounded = mag.toFixed(3)
+                  return 'Magnitude: ' + magRounded;
+                },     
+                                         
+             labelTextColor: function(tooltipItem, chart) {
+              return 'rgb(0,0,0)';
+                },                      
+            },
+            xPadding: 15,
+            yPadding: 15,
+            backgroundColor: 'rgb(255, 255, 255)',
+            titleFontSize: 18,
+            titleFontColor: 'rgb(0,0,0)',
+            bodyFontSize: 15,
+            bodyFontFamily: "'Helvetica', 'Arial', sans-serif"
+           },
             title: {
               display: true,
               fontSize: 24,
